dca77224d8a953686a476bb0a6fcdac10b8941d1
yuuta <yuuta_cookie@icloud.com>
2020-03-12 09:47:58 +0900

    30文字までの制約を追加

--- src/todo/dto/todo.dto.ts
+++ src/todo/dto/todo.dto.ts
@@ -1,13 +1,10 @@
 import { ObjectType, Field, ID } from 'type-graphql';
-import { MaxLength } from 'class-validator';
 
-// データ転送オブジェクト
 @ObjectType()
 export class TodoDto {
   @Field(() => ID)
   readonly id: number;
 
-  @MaxLength(20)
   @Field()
   readonly title: string;
 }
--- src/todo/inputs/createTodoInput.ts
+++ src/todo/inputs/createTodoInput.ts
@@ -1,9 +1,12 @@
 import { InputType, Field } from 'type-graphql';
+import { MaxLength } from 'class-validator';
 
 type input = { title: string };
 
 @InputType()
 export class CreateTodoInput implements input {
+  // 30文字まで
+  @MaxLength(30)
   @Field()
   title: string;
 }
