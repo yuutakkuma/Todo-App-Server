d4e3c1327fdae55a2e1ecba7d914f9df88e1d100
yuuta <yuuta_cookie@icloud.com>
2020-03-30 13:27:33 +0900

    テストをしやすいようにリファクタリングを実行

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -5,14 +5,19 @@ import { CreateTodoInput } from './inputs/createTodoInput';
 import { Todo } from './entity/todo.entity';
 import { GetToken } from '../customDecorator/getToken';
 import { TodoDto } from './dto/todo.dto';
+import { AuthService } from 'src/auth/auth.service';
 
 @Resolver('Todo')
 export class TodoResolver {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(
+    private readonly todoService: TodoService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Query(() => TodoDto)
   async getTodoList(@GetToken() token: string) {
-    return await this.todoService.todoList(token);
+    const payload = await this.authService.verify(token);
+    return await this.todoService.todoList(payload);
   }
 
   @Mutation(() => Boolean)
@@ -20,17 +25,12 @@ export class TodoResolver {
     @Args('input') input: CreateTodoInput,
     @GetToken() token: string,
   ) {
-    await this.todoService.create(input, token);
-    return true;
+    const payload = await this.authService.verify(token);
+    return await this.todoService.create(input, payload);
   }
 
   @Mutation(() => Boolean)
   async deleteTodo(@Args('id') id: number) {
-    const data = await Todo.findOne({ where: { id } });
-    if (!data) {
-      return false;
-    }
-    await this.todoService.delete(data);
-    return true;
+    return await this.todoService.delete(id);
   }
 }
