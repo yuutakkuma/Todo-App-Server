01667e8f14d3ed31380daa9c47155e2a6e6f91e0
yuuta <yuuta_cookie@icloud.com>
2020-04-05 20:05:03 +0900

    ニックネームの上限を20に変更

--- src/user/inputs/registerInput.ts
+++ src/user/inputs/registerInput.ts
@@ -4,7 +4,7 @@ import { InputType, Field } from 'type-graphql';
 @InputType()
 export class RegisterInput {
   @Field()
-  @Length(1, 30, { message: 'ニックネームは1文字以上、30文字以下です。' })
+  @Length(1, 20, { message: 'ニックネームは1文字以上、20文字以下です。' })
   nickName: string;
 
   @Field()
