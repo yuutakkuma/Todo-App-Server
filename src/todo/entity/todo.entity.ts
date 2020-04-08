79144e9d0efc661454cc1e49a557414fe2fce06d
yuuta <yuuta_cookie@icloud.com>
2020-04-08 16:27:37 +0900

    テーブル名を指定

--- src/todo/entity/todo.entity.ts
+++ src/todo/entity/todo.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
 
-@Entity({ synchronize: true })
+@Entity('todos', { synchronize: true })
 export class Todo extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
